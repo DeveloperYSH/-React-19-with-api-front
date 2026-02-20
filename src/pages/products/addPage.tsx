fe7d5295e3df934c252b01307a69ee12e2a5069e
ysh <didtkdgusqk@gmail.com>
2026-02-20 14:36:07 +0900

    상품 등록

--- src/pages/products/addPage.tsx
+++ src/pages/products/addPage.tsx
@@ -1,7 +1,12 @@
+import AddComponent from "../../components/products/addComponent";
+
 const AddPage = () => {
   return (
     <div className="p-4 w-full bg-white">
       <div className="text-3xl font-extrabold">Products Add Page</div>
+      <div>
+        <AddComponent />
+      </div>
     </div>
   );
 };
--- src/router/productsRouter.tsx
+++ src/router/productsRouter.tsx
@@ -1,9 +1,12 @@
 import { lazy, Suspense } from "react";
 import { Navigate } from "react-router";
+import { loadProducts } from "../pages/products/listPage";
 const ProductsIndex = lazy(() => import("../pages/products/indexPage"));
 const Loading = () => <div>Products Loading....</div>;
 
 const ProductsList = lazy(() => import("../pages/products/listPage"));
+
+const ProductsAdd = lazy(() => import("../pages/products/addPage"));
 
 export default function productsRouter() {
   return {
@@ -17,8 +20,17 @@ export default function productsRouter() {
             <ProductsList />
           </Suspense>
         ),
+        loader: loadProducts,
       },
       { path: "", element: <Navigate to={"/products/list"}></Navigate> },
+      {
+        path: "add",
+        element: (
+          <Suspense fallback={<Loading />}>
+            <ProductsAdd />
+          </Suspense>
+        ),
+      },
     ],
   };
 }
